2d8d4669e9b9ab800aedc59d26fae816940c6fc2
Ian Robinson <ianrrob2@aol.com>
2022-02-04 20:23:53 -0500

    Navbar update

--- django-issue-react/frontend/src/components/navbar/Navbar.jsx
+++ django-issue-react/frontend/src/components/navbar/Navbar.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import React, {useState} from 'react';
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri'
 import './navbar.css';
 import logo from '../../assets/issue.png'
 
-
-
+const Menu = () =>(
+  <>
+  <p><a href="#admin"></a>Admin</p>
+  <p><a href="#issues"></a>Issues</p>
+  </>
+    
+)
 
 const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false);
+
   return( 
   <div className='issue__navbar'>
     <div className='issue__navbar-links'>
       <div className='issue__navbar-links_logo'>
-          <img src={logo} alt="logo" width='75'></img>
+          <img src={logo} alt="logo"></img>
       </div>
+      <div className='issue__navbar-links_container'>
+          <Menu/>
+      </div>
+    </div>
+    <div className='issue__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'> Sign up</button>
+    </div>
+    <div className='issue__navbar-menu'>
+      {toggleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
+      }
+      {toggleMenu &&(
+        <div className='issue__navbar-menu_container scale-up-center' >
+          <div className='issue__navbar-menu_container-links'>
+            <Menu />
+            <div className='issue__navbar-menu_container-links-sign'>
+            <p>Sign in</p>
+            <button type='button'> Sign up</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   </div>
   )};
